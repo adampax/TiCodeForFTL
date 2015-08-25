8122a2d421a9e707929699d962c61ad533f5465d
Adam Paxton <adampaxton@polancomedia.com>
2015-08-25 16:05:36 -0400

    add projects and meetups tabs

---
+++ app/controllers/projects/projectsHome.js
@@ -0,0 +1,28 @@
+var args = arguments[0] || {};
+
+
+//add some animation when the window is first opened
+
+$.logo.animate({
+	opacity:1,
+	duration:700
+});
+
+$.container.animate({
+	opacity:1,
+	top:0,
+	duration: 1000
+});
+
+
+
+
+
+
+
+//our event listeners wired to each control in xml
+
+function openMap() {
+}
+
+
